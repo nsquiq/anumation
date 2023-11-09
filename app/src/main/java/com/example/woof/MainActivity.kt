4fe1f4c39a9d20bc99569dd4d7d5fec8bf27aaa3
Nuray <210103331@stu.sdu.edu.kz>
2023-11-09 16:21:24 +0600

    2

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -107,31 +107,48 @@ fun WoofApp() {
 fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
-)
-{ var expanded by remember { mutableStateOf(false) }
+) {
+    var expanded by remember { mutableStateOf(false) }
 
     Card(modifier = modifier) {
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_small))
-        ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
-            Spacer(modifier = Modifier.weight(1f))
-            DogItemButton(expanded = expanded,
-                onClick = {})
+        Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_small))
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(expanded = expanded,
+                    onClick = {})
+            }
+            DogHobby(
+                dog.hobbies,
+                modifier = Modifier.padding(
+                    start = dimensionResource(R.dimen.padding_medium),
+                    top = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_medium),
+                    bottom = dimensionResource(R.dimen.padding_medium)
+                ))
         }
     }
 }
-
 @Composable
 fun DogHobby(
     @StringRes dogHobby: Int,
     modifier: Modifier = Modifier
 ){
-
+  Column(modifier = modifier){
+      Text(
+          text = stringResource(R.string.about),
+          style = MaterialTheme.typography.labelSmall
+      )
+      Text(
+          text = stringResource(dogHobby),
+          style = MaterialTheme.typography.bodyLarge
+      )
+  }
 }
 /**
  * Composable that displays a photo of a dog.
